9916f1af917ea53a0161eef3e0ee773c3f2abb2b
ASSEMBE ENGHOT Winny Yanel <119531545+vinnyyanel@users.noreply.github.com>
2024-05-29 14:54:58 +0100

    porte folio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path:'',
+    redirectTo:'/home',
+    pathMatch:'full'
+  },
+  {
+    path:'home',
+    title:'home',
+    component:AppComponent
+  }
+];
